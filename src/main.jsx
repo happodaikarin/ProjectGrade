cbd9ba7c087a61bac9748f0a3575b63309c46ca5
happodaikarin <rambandan@gmail.com>
2024-03-06 17:02:24 -0500

    nueva modificacion main

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,15 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import ReactGA from "react-ga4";
+
+
+ReactGA.initialize("G-NL6TRG7MES");
+
+ReactGA.send({
+  hitType: "pageview",
+  page: window.location.pathname,
+});
 
 const container = document.getElementById('root');
 const root = createRoot(container); // createRoot(container!) si estás usando TypeScript
